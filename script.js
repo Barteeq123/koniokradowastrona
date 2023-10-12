840ff41d6fc7c06aebdd8eecdf155c52625d5ddb
Barteeq123 <lubie.tory2012g@gmail.com>
2023-10-12 16:49:52 +0200

    Script improvement.

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 function wysylanieKoniokrada() {
-    let kon;
-    let losowanie_konia = Math.floor(Math.random() * 5);
-    if (losowanie_konia == 0) {
-        kon = "images/koniokrad.png";
-    }
-    else if (losowanie_konia == 1) {
-        kon = "images/koniokrad4.png";
-    }
-    else if (losowanie_konia == 2) {
-        kon = "images/koniokrad3.png";
-    }
-    else if (losowanie_konia == 3) {
-        kon = "images/koniokrad5.png";
-    }
-    else if (losowanie_konia == 4) {
-        kon = "images/koniokrad6.png";
-    }
-    return kon;
+    return "images/koniokrad" + Math.floor(Math.random() * 5) + ".png";
 }
 
 document.getElementById("img").src = wysylanieKoniokrada();
